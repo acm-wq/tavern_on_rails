edab058c394dffbfb2666e000c920d66b8acf36e
acm-wq <camisglhdadae@gmail.com>
2025-01-05 18:09:35 +0700

    feat: add user login functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   resources :users, only: [:create]
+  resources :sessions, only: [:create]
 
   get 'register', to: 'users#new', as: 'register'
+  get 'login', to: 'sessions#new', as: 'login'
+  delete 'logout', to: 'sessions#destroy', as: 'logout'
 
-  # Defines the root path route ("/")
   root "users#new"
 end
