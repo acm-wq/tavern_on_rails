121f53a8d1db6723ea72b3e37c56a99ad0743ffc
acm-wq <camisglhdadae@gmail.com>
2025-01-13 00:31:31 +0700

    main 🍺 fix user role

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,4 +1,4 @@
 class Assignment < ApplicationRecord
-  belongs_to :user
-  belongs_to :user_role
+  belongs_to :user, foreign_key: "users_id"
+  belongs_to :user_role, foreign_key: "user_roles_id"
 end
--- app/models/user_role.rb
+++ app/models/user_role.rb
@@ -1,5 +1,5 @@
 class UserRole < ApplicationRecord
-  has_many :assignments
+  has_many :assignments, foreign_key: "user_roles_id"
   has_many :users, through: :assignments
 
   validates :name, presence: true, uniqueness: true
